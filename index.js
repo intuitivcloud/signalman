8cd9efa04b3d67d8dcaa2940ef8ad239da65b3fd
codematix <codematix@codematix.me>
2015-06-13 23:40:19 +0530

    implement client-side routing

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ var microevent = require('microevent'),
     paqs = require('paqs'),
     u = require('./utils');
 
-var httpSafeMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE'];
+var httpSafeMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE'],
+    isBrowser = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
 
 /**
  * A router that routes requests
+ *
+ * @public
  *
  * @class
  * @constructor
@@ -34,6 +37,8 @@ httpSafeMethods.reduce(function (proto, method) {
 
   /**
    * Registers the specified chain of handlers for the specified path and method
+   *
+   * @public
    *
    * @param  {string} path - the path to handle
    * @param  {function[]} handlers - one or more handlers
@@ -58,6 +63,8 @@ httpSafeMethods.reduce(function (proto, method) {
 /**
  * Finds and returns the first route whose matcher matches the specified
  *
+ * @private
+ *
  * @param  {string} method - the HTTP method to match route against
  * @param  {string} path - the path to match the route against
  *
@@ -70,20 +77,29 @@ Router.prototype._findRoute = function findRoute(method, path) {
 };
 
 /**
- * The server-side request dispatcher
+ * Builds and returns a new context object for the specified path, route and state
  *
- * @param {Object}   req  - the request object sent by the server
- * @param {Object}   res  - the response object sent by the server
- * @param {Function} next - the next function in the server middleware chain
+ * @private
+ *
+ * @param {string} path - the path for which this context is to be created
+ * @param {Route} route - the route object that will be associated with the context
+ * @param {Function} next - the next handler
+ * @param {Object} [state] - an optional state with additional attributes to add to context
+ *
+ * @return {Context} a new instance of the navigation context object
  */
-Router.prototype.serverDispatcher = function (req, res, next) {
-  var parsedUrl = purl(req.url),
-      path = parsedUrl.pathname,
-      cause = 'httpRequest',
-      route, cxt, handlerQ;
+Router.prototype._createContext = function _createContext(path, route, next, state) {
+  var handlerQ = route.handlers.slice(),
+      cxt = u.merge({
+        path: path,
+        router: this,
+        canUseDOM: isBrowser
+      }, state);
 
   /**
    * The next middleware iterator internal to signalman
+   *
+   * @public
    *
    * @return {Function} the next middleware function registered for the route
    */
@@ -95,6 +111,26 @@ Router.prototype.serverDispatcher = function (req, res, next) {
     return handler(cxt);
   }
 
+  cxt.next = nextHandler;
+
+  return cxt;
+};
+
+/**
+ * The server-side request dispatcher
+ *
+ * @private
+ *
+ * @param {Object}   req  - the request object sent by the server
+ * @param {Object}   res  - the response object sent by the server
+ * @param {Function} next - the next function in the server middleware chain
+ */
+Router.prototype._serverDispatcher = function (req, res, next) {
+  var parsedUrl = purl(req.url),
+      path = parsedUrl.pathname,
+      cause = 'httpRequest',
+      route, cxt;
+
   // find the route
   route = this._findRoute(req.method, path);
 
@@ -105,26 +141,121 @@ Router.prototype.serverDispatcher = function (req, res, next) {
   req.params = route.matcher(path);
   req.query = parsedUrl.search ? paqs(parsedUrl.search) : {};
 
-  handlerQ = route.handlers.slice();            // shallow copy of handlers, queued
-
-  cxt = {
+  // build new context
+  cxt = this._createContext(path, route, next, {
     cause: cause,
-    path: path,
-    router: this,
     request: req,
-    response: res,
-    next: nextHandler
-  };
+    response: res
+  });
 
   // trigger a navigating event
   this.trigger('navigating', {path: path, cause: cause, router: this});
 
-  return nextHandler();
+  return cxt.next();
 };
 
-Router.prototype.start = function () {
-  return this.serverDispatcher.bind(this);
+/**
+ * The client-side dispatcher
+ *
+ * @private
+ *
+ * @param {string} path - the path to dispatch to
+ * @param {Object} [state] - an optional state to be associated with the navigation
+ */
+Router.prototype._clientDispatcher = function (path, state) {
+  var currPath = document.location.pathname,
+      url = purl(path),
+      route = this._findRoute('GET', url.pathname),
+      cxt, newState;
+
+  if (!route) {
+    this.trigger('notFound', path);
+    return;
+  }
+
+  // build new context
+  cxt = this._createContext(url.pathname, route, u.noop, {
+    fullPath: path,
+    cause: (state.cause || 'navigation'),
+    params: (state.params || route.matcher(url.pathname)),
+    query: (state.query || paqs(url.search))
+  });
+
+  // cherry-pick only serializable stuff from context
+  newState = u.pick(cxt, ['fullPath', 'path', 'params', 'query', 'cause']);
+
+  if (currPath === url.pathname)
+    window.history.replaceState(newState, null, path);
+  else
+    window.history.pushState(newState, null, path);
+
+  // invoke the first handler/middleware
+  cxt.next();
 };
+
+/**
+ * The client-side popState event handler
+ *
+ * @private
+ *
+ * @param {Object} e - the popState event object
+ */
+Router.prototype._onPopState = function (e) {
+  var state = e.state;
+  this._clientDispatcher(state.fullPath, state);
+};
+
+/**
+ * Starts routing
+ *
+ * @public
+ *
+ * @param {Object} opts - options for routing
+ */
+Router.prototype.start = function (opts) {
+  opts = opts || { autoStart: false };
+
+  // already running, noop
+  if (this._isStarted) return undefined;
+
+  // if on server, use server dispatcher
+  if (!isBrowser) return this._serverDispatcher.bind(this);
+
+  this._popStateHandler = this._onPopState.bind(this);
+
+  // attach client dispatcher
+  window.addEventListener('popstate', this._popStateHandler);
+
+  // navigate to current document URL if autoStart specified
+  if (opts.autoStart)
+    this.navigateTo(document.location.href, { cause: 'startup' });
+
+  this._started = true;
+};
+
+/**
+ * Stops routing client-side navigation
+ *
+ * @public
+ */
+Router.prototype.stop = function stopRouting() {
+  if (!(isBrowser && this._isStarted)) return;
+
+  window.removeEventListener('popstate', this._popStateHandler);
+
+  this._isStarted = false;
+};
+
+// add the navigateTo method only if we are in a browser
+if (isBrowser)
+  /**
+   * Triggers navigation to the specified path
+   *
+   * @public
+   */
+  Router.prototype.navigateTo = function (path) {
+    this._clientDispatcher(path, {cause: 'navigation'});
+  };
 
 module.exports = function signalman() {
   return new Router();
